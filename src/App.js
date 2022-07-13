2f1dcb83601bf06ab207d8a05f4b81018af1b2bf
João Pedro Belo <98714001+silvajpedro@users.noreply.github.com>
2022-07-12 21:17:11 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+export default function desafio() {
+  return (
+    <div>
+      <h1>Estou usando o function component</h1>
+      <h2>E importei o CSS</h2>
+    </div>
+  );
+}
